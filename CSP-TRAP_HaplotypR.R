d5eaac6b3a1a59a8c28a0745f857a40169806314
leenvh <leen.vanheer@lshtm.ac.uk>
2023-09-12 09:58:30 +0000

    First commit

---
+++ CSP-TRAP_HaplotypR.R
@@ -0,0 +1,195 @@
+library("HaplotypR")
+library("ShortRead")
+
+# Define output directory
+outputDir <- "/mnt/storage9/leen/MOI_ampliconseq/12_09_23_CSP-TRAP_0.005"
+# Create output directory
+if(!dir.exists(outputDir))
+  dir.create(outputDir, recursive=T)
+
+# source edited funs
+source("/mnt/storage9/leen/MOI_ampliconseq/scripts/deplexBySample_ext.R")
+source("/mnt/storage9/leen/MOI_ampliconseq/scripts/checkBarcode_ext.R")
+
+##############Run demultiplexing by sample 
+# Define a function to perform demultiplexing
+demultiplex <- function(pattern, outputDir, sampleNum) {
+  # Set input file path
+  primerFile <- paste0("/mnt/storage9/leen/MOI_ampliconseq/primerfiles/markerFile_CSP_TRAP.txt")
+  sampleFile <- paste0("/mnt/storage9/leen/MOI_ampliconseq/samplefiles/", "sampleFile_", pattern, ".txt")
+  fnBarcodeF <- paste0("/mnt/storage9/leen/MOI_ampliconseq/barcodefiles/barcode_Fwd.fasta")
+  fnBarcodeR <- paste0("/mnt/storage9/leen/MOI_ampliconseq/barcodefiles/barcode_Rev.fasta")
+  
+  # Set reads
+  reads <- list.files("/mnt/storage9/leen/MOI_ampliconseq/fastq/", pattern = paste0(pattern, "_R"), full.names = TRUE)
+  
+  # Create output subdirectory
+  outDeplexSample <- file.path(outputDir, paste0("dePlexSample", sampleNum))
+  dir.create(outDeplexSample, showWarnings = FALSE)
+  
+  # Demultiplex by samples - no SNPs or deletions allowed
+  dePlexSample <- deplexSampleExtended(reads[1], reads[2], fnBarcodeF, fnBarcodeR, outDeplexSample, max.mismatch = 0, with.indels = FALSE)
+  
+  sampleTab <- read.delim(sampleFile, stringsAsFactors = FALSE)
+  dePlexSample <- renameDemultiplexedFiles(sampleTab, dePlexSample)
+  
+  write.table(dePlexSample, file.path(outputDir, paste0("demultiplex", sampleNum, "SampleSummary.txt")), sep = "\t", row.names = FALSE)
+  dePlexSample <- na.omit(dePlexSample)
+}
+
+# Set the folder path to fastq files
+folder_path <- "/mnt/storage9/leen/MOI_ampliconseq/fastq/"
+
+# List all the files in the folder
+file_list <- list.files(folder_path, full.names = TRUE)
+
+# Define a function to extract the sample name from the file name
+extract_sample_name <- function(file_name) {
+  # Extract the sample name from the file name (e.g., Pool1Rep1Oct22_Combined_R1)
+  # Modify this logic as needed based on your file naming pattern
+  sample_name <- gsub("_R[12].*\\.fastq\\.gz", "", file_name)
+  return(sample_name)
+}
+
+# Define a function to generate and execute demultiplex lines
+generate_and_execute_demultiplex_lines <- function(file_list, outputDir) {
+  # Iterate over the file list and generate demultiplex lines
+  for (i in 1:length(file_list)) {
+    file_name <- basename(file_list[i])
+    sample_name <- extract_sample_name(file_name)
+    demultiplex_line <- paste0("demultiplex(\"", sample_name, "\", outputDir, ", i, ")")
+    cat(demultiplex_line, "\n")
+    # Execute the generated line as R code
+    eval(parse(text = demultiplex_line))
+  }
+}
+
+# Call the function with your file_list and outputDir
+generate_and_execute_demultiplex_lines(file_list, outputDir)
+
+
+
+
+###################merge dataframes from the 40 pools with rbind
+# Create an empty list to store the dataframes
+dePlexSampleList <- list()
+
+# Loop to load dataframes and add them to the list
+for (i in 1:40) {
+  # Assuming your dataframes are named dePlexSample1, dePlexSample2, etc.
+  df_name <- paste0("dePlexSample", i)
+  
+  # Check if the dataframe exists in your environment
+  if (exists(df_name)) {
+    dePlexSampleList[[i]] <- get(df_name)
+  }
+}
+
+# Merge dataframes using do.call and rbind
+if (length(dePlexSampleList) > 0) {
+  dePlexSample <- do.call(rbind, dePlexSampleList)
+} else {
+  # Handle the case where no dataframes were found
+  cat("No dataframes found.")
+}
+
+write.table(dePlexSample, file.path(outputDir, "demultiplexSampleSummary.txt"), sep="\t", row.names=F)
+
+################Run demultiplex by marker and truncate primer sequence
+# create output subdirectory
+outDeplexMarker <- file.path(outputDir, "dePlexMarker")
+dir.create(outDeplexMarker)
+
+# process each marker
+markerTab <- read.delim(primerFile, stringsAsFactors=F)
+dePlexMarker <- demultiplexByMarker(dePlexSample, markerTab, outDeplexMarker)
+
+# save summary table
+write.table(dePlexMarker, file.path(outputDir, "demultiplexMarkerSummary.txt"), sep="\t", row.names=F)
+#remove lines without sampleID
+dePlexMarker <- na.omit(dePlexMarker)
+
+#############Second method work only for overlapping sequence read pair by merging the overlap of the forward and reverse read (using vsearch wrapper).
+# create output subdirectory
+outProcFiles <- file.path(outputDir, "processedReads")
+dir.create(outProcFiles)
+
+postfix <- "_merge"
+refSeq <- DNAStringSet(markerTab$ReferenceSequence)
+names(refSeq) <- markerTab$MarkerID
+lapply(seq_along(refSeq), function(i){
+  writeFasta(refSeq[i], file.path(outputDir, paste(names(refSeq)[i], postfix, ".fasta", sep="")))
+})
+
+procReadsMerge <- mergeAmpliconReads(as.character(dePlexMarker$FileR1), as.character(dePlexMarker$FileR2), outProcFiles)
+procReads <- cbind(dePlexMarker[,c("SampleID", "SampleName","BarcodePair", "MarkerID")], procReadsMerge)
+write.table(procReads, file.path(outputDir, sprintf("processedReadSummary%s.txt", postfix)), sep="\t", row.names=F, quote=F)
+
+# subset: remove markers without reads
+procReads <- procReads[procReads$numRead>10,]
+#procReads <- procReads[procReads$MarkerID %in% c("csp","trap","ama1"),]
+###############Calculate mismatch rate and call SNPs
+# Options
+minMMrate <- 0.5
+minOccGen <- 2
+
+# process each marker
+snpLst <- lapply(markerTab$MarkerID, function(marker){
+  # Calculate mismatch rate
+  seqErrLst <- calculateMismatchFrequencies(as.character(procReads[procReads$MarkerID == marker, "ReadFile"]),
+                                            refSeq[marker],
+                                            method ="pairwiseAlignment", # c("pairwiseAlignment","compareDNAString"),
+                                            minCoverage=100L)
+  names(seqErrLst) <- procReads[procReads$MarkerID == marker, "SampleID"]
+  seqErr <- do.call(cbind, lapply(seqErrLst, function(l){
+    l[,"MisMatch"]/l[,"Coverage"]
+  }))
+  write.table(seqErr, file.path(outputDir, sprintf("mismatchRate_rate_%s%s.txt", marker, postfix)), sep="\t", row.names=F)
+
+  # Call SNPs
+  potSNP <- callGenotype(seqErr, minMismatchRate=minMMrate, minReplicate=minOccGen)
+  snpRef <- unlist(lapply(potSNP, function(snp){
+    as.character(subseq(refSeq[marker], start=snp, width=1))
+  }))
+  snps <- data.frame(Chr=marker, Pos=potSNP, Ref=snpRef, Alt="N", stringsAsFactors=F)
+  write.table(snps, file=file.path(outputDir, sprintf("potentialSNPlist_rate%.0f_occ%i_%s%s.txt",
+                                                      minMMrate*100, minOccGen, marker, postfix)),
+              row.names=F, col.names=T, sep="\t", quote=F)
+
+  # Plot mismatch rate and SNP calls
+  png(file.path(outputDir, sprintf("plotMisMatchRatePerBase_rate%.0f_occ%i_%s%s.png",
+                                   minMMrate*100, minOccGen, marker, postfix)),
+      width=1500 , height=600)
+  matplot(seqErr, type="p", pch=16, cex=0.4, col="#00000088", ylim=c(0, 1),
+          ylab="Mismatch Rate", xlab="Base Position", main=marker, cex.axis=2, cex.lab=2)
+  abline(v=snps[,"Pos"], lty=2, col="grey")
+  abline(h=minMMrate, lty=1, col="red")
+  dev.off()
+
+  return(snps)
+})
+names(snpLst) <- markerTab$MarkerID
+
+##################call haplotypes
+# call haplotype options
+minCov <- 3
+detectionLimit <- 1/200
+minOccHap <- 2
+minCovSample <- 25
+
+# remove samples without reads
+procReads <- procReads[procReads$numRead>0,]
+
+source("/mnt/storage9/leen/MOI_ampliconseq/scripts/createHaplotypeTable_ext.R")
+
+# call final haplotypes
+finalTab <- createFinalHaplotypTableExtended(
+  outputDir = outputDir, sampleTable = procReads, markerTable = markerTab, referenceSeq = refSeq,
+  snpList = snpLst, postfix = postfix, minHaplotypCoverage = minCov, minReplicate = minOccHap,
+  detectability = detectionLimit, minSampleCoverage = minCovSample)
+
+CSP_finalTab<-finalTab[["csp"]]
+TRAP_finalTab<-finalTab[["trap"]]
+
+write.table(CSP_finalTab, file.path(outputDir, "CSP_finalTab.txt"), sep="\t", row.names=F, quote=F)
+write.table(TRAP_finalTab, file.path(outputDir, "TRAP_finalTab.txt"), sep="\t", row.names=F, quote=F)
